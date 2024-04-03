4019486d6feda036f664811deaaa5e70b7cfd282
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-04-03 11:31:58 +0600

    nav links created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
 
   return (
     <>
+      <DaisyNav></DaisyNav>
       <h1>Price Options</h1>
+      <NavBar></NavBar>
     </>
   )
 }
